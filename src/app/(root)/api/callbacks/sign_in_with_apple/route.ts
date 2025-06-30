9ceeab829234ddc2607ba654cb38ce27af38515d
KreatorDev <nowkreator@gmail.com>
2025-06-30 17:12:43 +0100

    fix

--- src/app/(root)/api/callbacks/sign_in_with_apple/route.ts
+++ src/app/(root)/api/callbacks/sign_in_with_apple/route.ts
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-  const query = request.nextUrl.searchParams;
-  const app_id = query.get("app_id");
-  if (!app_id) return NextResponse.error();
-  const body = await request.formData();
-  const params = new URLSearchParams();
-  body.forEach((value, key) => {
-    if (typeof value === "string") params.append(key, value);
-  });
-  const redirect = `intent://callback?${params.toString()}#Intent;package=${app_id};scheme=signinwithapple;end`;
-  return NextResponse.redirect(redirect, 307);
+export async function POST(req: NextRequest) {
+  try {
+    const query = req.nextUrl.searchParams;
+    const app_id = query.get("app_id");
+    if (!app_id) throw "App ID not found";
+    const body = await req.formData();
+    const params = new URLSearchParams();
+    body.forEach((value, key) => {
+      if (typeof value === "string") params.append(key, value);
+    });
+    const redirect = `intent://callback?${params.toString()}#Intent;package=${app_id};scheme=signinwithapple;end`;
+    return NextResponse.redirect(redirect, 307);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 401 });
+  }
 }
